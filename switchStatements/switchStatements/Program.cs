aaf053d3ce3e4a5f6996c8aaf3562c2ad925486c
theoshi21 <143781578+theoshi21@users.noreply.github.com>
2023-09-29 15:42:33 +0800

    Add files via upload

---
+++ switchStatements/switchStatements/Program.cs
@@ -0,0 +1,20 @@
+﻿int val1, val2;
+
+Console.Write("Insert number 1: ")
+val1 = int.Parse(Console.ReadLine());
+
+Console.Write("Inser number 2: ")
+val2 = int.Parse(Console.ReadLine());
+
+switch(val1 > val2)
+{
+    case true:
+        Console.WriteLine("The maximum value is " + val1);
+        break;
+    case false:
+        Console.WriteLine("The maximum value is " + val2);
+        break;
+    default:
+        Console.WriteLine("Invalid input.");
+        break;
+}
